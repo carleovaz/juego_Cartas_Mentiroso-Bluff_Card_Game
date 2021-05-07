5c439852bf3f618e7959a8dd63e667dd7a8c5f86
carleovaz <carleovaz@gmail.com>
2021-05-07 18:57:22 +0200

    Actualizado

--- src/es/Studium/Mentiroso/Controlador.java
+++ src/es/Studium/Mentiroso/Controlador.java
@@ -1,13 +1,118 @@
 package es.Studium.Mentiroso;
 
-public class Controlador 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Controlador implements ActionListener, WindowListener
 {
 	Vista vista;
 	Modelo modelo;
-	
+
 	public Controlador(Vista objvista, Modelo objmodelo)
 	{
 		this.vista = objvista;
 		this.modelo = objmodelo;
+
+		objvista.ventanaPrincipal.addWindowListener(this);
+		objvista.ventanaCrear.addWindowListener(this);
+		objvista.ventanaComoSeJuega.addWindowListener(this);
+		objvista.ventanaMejoresJugadores.addWindowListener(this);
+
+		objvista.buttonCrear.addActionListener(this);
+		objvista.buttonMejores.addActionListener(this);
+		objvista.buttonSalir.addActionListener(this);
+		objvista.cancelar.addActionListener(this);
+		objvista.comoseJuega.addActionListener(this);
+		objvista.cerrar.addActionListener(this);
+
+		objvista.addWindowListener(this);
+		objvista.setLocationRelativeTo(null);
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent evento) 
+	{
+
+		if(vista.buttonCrear.equals(evento.getSource()))
+		{
+			vista.ventanaCrear.setVisible(true);
+		}
+
+		else if(vista.buttonMejores.equals(evento.getSource()))
+		{
+			vista.ventanaMejoresJugadores.setVisible(true);
+		}
+
+		else if(vista.comoseJuega.equals(evento.getSource()))
+		{
+			vista.ventanaComoSeJuega.setVisible(true);
+		}
+		
+		if(vista.buttonSalir.equals(evento.getSource()))
+		{
+			System.exit(0);
+		}
+
+		else if(vista.cancelar.equals(evento.getSource()))
+		{
+			vista.ventanaCrear.setVisible(false);
+		}
+
+		else if(vista.cerrar.equals(evento.getSource()))
+		{
+			vista.ventanaMejoresJugadores.setVisible(false);
+		}
+
+	}
+
+
+	public void windowClosing(WindowEvent arg0) 
+	{
+		vista.ventanaPrincipal.setVisible(false);
+		vista.ventanaCrear.setVisible(false);
+		vista.ventanaMejoresJugadores.setVisible(false);
+		vista.ventanaComoSeJuega.setVisible(false);
+
+	}
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+
 }
--- src/es/Studium/Mentiroso/Vista.java
+++ src/es/Studium/Mentiroso/Vista.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Label;
 import java.awt.TextArea;
+import java.awt.TextField;
 
 public class Vista  extends Frame
 {
@@ -18,8 +19,9 @@ public class Vista  extends Frame
 	Button buttonSalir = new Button ("Salir");//CIERRA EL PROGRAMA
 	
 	//VENTANA CREAR PARTIDA
-	Frame ventanCrear = new Frame ("¡El Mentiroso!: Crear Partida");
+	Frame ventanaCrear = new Frame ("¡El Mentiroso!: Crear Partida");
 	Label labelNombrePartida = new Label ("Nombre de la Partida");
+	TextField textoNombre = new TextField (25);
 	Button crearPartida = new Button ("Crear"); //AL CREAR PARTIDA, INICIAMOS EL JUEGO Y SE GUARDA EL NOMBRE
 	Button cancelar = new Button ("Cancelar"); //CIERRA LA PANTALLA DE CREAR
 	Button comoseJuega = new Button ("¿Como se juega?"); //ABRE LA VENTANA DE COMO SE JUEGA
@@ -28,13 +30,16 @@ public class Vista  extends Frame
 	Frame ventanaMejoresJugadores = new Frame("Consulta");
 	Label labelMejores = new Label ("¡Mejores Mentirosos!");
 	TextArea listadoJugadores = new TextArea(5, 30);
-	Button volver = new Button("Volver"); //CIERRA LA VENTANA DE MEJORES JUGADORES
+	Button cerrar = new Button("cerrar"); //CIERRA LA VENTANA DE MEJORES JUGADORES
 	
 	//VENTANA COMO SE JUEGA
 	Frame ventanaComoSeJuega = new Frame("Consulta");
-	TextArea ayuda1 = new TextArea(5, 30);//PONER IMAGEN AL LADO
-	TextArea ayuda2 = new TextArea(5, 30);//PONER IMAGEN AL LADO
-	TextArea ayuda3 = new TextArea(5, 30);//PONER IMAGEN AL LADO
+	Label labelAyuda1 = new Label ("info de ayuda 1");//TITULO DE LA AYUDA 1
+	TextArea ayuda1 = new TextArea(10, 60);//PONER IMAGEN AL LADO 
+	Label labelAyuda2 = new Label ("info de ayuda 2");//TITULO DE LA AYUDA 2
+	TextArea ayuda2 = new TextArea(10, 60);//PONER IMAGEN AL LADO
+	Label labelAyuda3 = new Label ("info de ayuda 3");//TITULO DE LA AYUDA 3
+	TextArea ayuda3 = new TextArea(10, 60);//PONER IMAGEN AL LADO
 	
 
 	public Vista()
@@ -49,10 +54,35 @@ public class Vista  extends Frame
 		ventanaPrincipal.setResizable(false);
 		ventanaPrincipal.setLocationRelativeTo(null);
 		
+		ventanaCrear.setLayout(new FlowLayout());
+		ventanaCrear.add(labelNombrePartida);
+		ventanaCrear.add(textoNombre);
+		ventanaCrear.add(crearPartida);
+		ventanaCrear.add(cancelar);
+		ventanaCrear.add(comoseJuega);
+		ventanaCrear.setSize(300,200);
+		ventanaCrear.setLocationRelativeTo(null);
+		ventanaCrear.setResizable(false);
+		
+		ventanaMejoresJugadores.setLayout(new FlowLayout());
+		ventanaMejoresJugadores.add(labelMejores);
+		ventanaMejoresJugadores.add(listadoJugadores);
+		ventanaMejoresJugadores.add(cerrar);
+		ventanaMejoresJugadores.setSize(300,220);
+		ventanaMejoresJugadores.setLocationRelativeTo(null);
+		ventanaMejoresJugadores.setResizable(false);
+		
+		ventanaComoSeJuega.setLayout(new FlowLayout());
+		ventanaComoSeJuega.add(ayuda1);
+		ventanaComoSeJuega.add(labelAyuda1);
+		ventanaComoSeJuega.add(ayuda2);
+		ventanaComoSeJuega.add(labelAyuda2);
+		ventanaComoSeJuega.add(ayuda3);
+		ventanaComoSeJuega.add(labelAyuda3);
+		ventanaComoSeJuega.setSize(950,600);
+		ventanaComoSeJuega.setLocationRelativeTo(null);
+		ventanaComoSeJuega.setResizable(false);
+			
 	}
 	
-//	public static void main (String[]args)
-//	{
-//		new Vista();
-//	}
 }
